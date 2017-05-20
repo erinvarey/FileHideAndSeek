523c6bd0b2412b090c2e2208c38963dfe432f454
erinvarey <vareye@mcmaster.ca>
2017-05-19 22:21:29 -0400

    trying to randomize new filke name

--- HIDE_AND_SEEK/src/main.java
+++ HIDE_AND_SEEK/src/main.java
@@ -1,4 +1,5 @@
 import java.io.File;
+import java.util.Random;
 import java.util.Scanner;
 
 public class main {
@@ -23,7 +24,24 @@ public class main {
 	public static String pickfile(String s){
 		File folder = new File(s);
 		File[] listOfFiles = folder.listFiles();
+		int index = (int) Math.floor((Math.random()*listOfFiles.length));
+		listOfFiles[index]=renameTo("C:/Users/Erin/Desktop/TEST/moved.txt"+filename());
 		return "";
+	}
+
+	public static Object filename(){
+		String charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz()[]{}^*!@#$";
+		int numChars = 64;
+		int length = 20;
+		Random rand = new Random();
+		File File = new File("");
+		String filename = File.getName();
+		String base = FilenameUtils.removeExtension(filename);
+        for (int i = 0; i < length; i++){
+            int index = (int) (rand.nextFloat() * numChars);
+            File = File(charset.charAt(index));
+        }
+        return File;
 	}
 
 }
